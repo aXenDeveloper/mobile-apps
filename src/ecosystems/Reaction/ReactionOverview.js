a8efc4f62ea9ff21a44bcbb12e555041bc500694
Rikki Tissier <rtissier@invisionpower.com>
2018-08-26 19:07:03 -0400

    Fix for ReactionOverview showing reactions back to front

--- src/ecosystems/Reaction/ReactionOverview.js
+++ src/ecosystems/Reaction/ReactionOverview.js
@@ -7,9 +7,13 @@ export default class ReactionOverview extends Component {
 	}
 
 	render() {
+		// Clone and reverse reaction data so that we can show them layered on top of each other
+		// Style uses row-reverse to put them back in the right order
+		const reactions = this.props.reactions.slice(0).reverse();
+
 		return (
 			<View style={[componentStyles.wrapper, this.props.style]}>
-				{this.props.reactions.map( (reaction, idx) => <Image source={{ uri: reaction.image }} key={reaction.id} style={[componentStyles.reaction, idx === 0 ? componentStyles.first : null]} />)}
+				{reactions.map( (reaction, idx) => <Image source={{ uri: reaction.image }} key={reaction.id} style={[componentStyles.reaction, idx === 0 ? componentStyles.first : null]} />)}
 			</View>
 		);
 	}
