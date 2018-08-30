03df59e9010cdf6a7376c8a1243b9de908024f2c
Rikki Tissier <rtissier@invisionpower.com>
2018-08-30 17:54:08 -0400

    JSPrettier

--- src/ecosystems/ContentCard/index.js
+++ src/ecosystems/ContentCard/index.js
@@ -1,19 +1,9 @@
 import React, { Component } from "react";
-import {
-	Text,
-	View,
-	Image,
-	StyleSheet,
-	TouchableHighlight,
-	TouchableOpacity
-} from "react-native";
+import { Text, View, Image, StyleSheet, TouchableHighlight, TouchableOpacity } from "react-native";
 import FadeIn from "react-native-fade-in-image";
 
 import Lang from "../../utils/Lang";
-import {
-	PlaceholderContainer,
-	PlaceholderElement
-} from "../../ecosystems/Placeholder";
+import { PlaceholderContainer, PlaceholderElement } from "../../ecosystems/Placeholder";
 import { styleVars } from "../../styles";
 
 export default class ContentCard extends Component {
@@ -24,74 +14,35 @@ export default class ContentCard extends Component {
 	render() {
 		if (this.props.loading) {
 			return (
-				<View
-					style={[
-						componentStyles.contentCard,
-						this.props.style,
-						{ height: 300 }
-					]}
-				>
+				<View style={[componentStyles.contentCard, this.props.style, { height: 300 }]}>
 					<PlaceholderElement circle radius={20} left={12} top={12} />
-					<PlaceholderElement
-						width={150}
-						left={40}
-						top={16}
-						height={10}
-					/>
-					<PlaceholderElement
-						width="100%"
-						left={0}
-						right={0}
-						top={45}
-						height={135}
-					/>
+					<PlaceholderElement width={150} left={40} top={16} height={10} />
+					<PlaceholderElement width="100%" left={0} right={0} top={45} height={135} />
 					{this.props.image && <PlaceholderElement width={200} left={12} top={192} />}
-					<PlaceholderElement
-						width={150}
-						left={12}
-						top={215}
-						height={12}
-					/>
-					<PlaceholderElement
-						width={250}
-						left={12}
-						top={240}
-						height={12}
-					/>
-					<PlaceholderElement
-						width={250}
-						left={12}
-						top={256}
-						height={12}
-					/>
-					<PlaceholderElement
-						width={250}
-						left={12}
-						top={272}
-						height={12}
-					/>
+					<PlaceholderElement width={150} left={12} top={215} height={12} />
+					<PlaceholderElement width={250} left={12} top={240} height={12} />
+					<PlaceholderElement width={250} left={12} top={256} height={12} />
+					<PlaceholderElement width={250} left={12} top={272} height={12} />
 				</View>
 			);
 		}
 
 		return (
-			<TouchableOpacity activeOpacity={styleVars.touchOpacity} style={[componentStyles.contentCard, this.props.style]} onPress={this.props.onPress || null}>
+			<TouchableOpacity
+				activeOpacity={styleVars.touchOpacity}
+				style={[componentStyles.contentCard, this.props.style]}
+				onPress={this.props.onPress || null}
+			>
 				<View style={[componentStyles.contentCardInner]}>
 					{this.props.header && (
 						<View style={componentStyles.streamHeader}>
 							<View style={componentStyles.streamMeta}>
-								<View style={componentStyles.streamMetaInner}>
-									{this.props.header}
-								</View>
+								<View style={componentStyles.streamMetaInner}>{this.props.header}</View>
 							</View>
 						</View>
 					)}
 					{this.props.image && this.props.image}
-					{this.props.content && (
-						<View style={[componentStyles.streamFooter]}>
-							{this.props.content}
-						</View>
-					)}
+					{this.props.content && <View style={[componentStyles.streamFooter]}>{this.props.content}</View>}
 				</View>
 			</TouchableOpacity>
 		);
@@ -113,9 +64,9 @@ const componentStyles = StyleSheet.create({
 	},
 	contentCardInner: {
 		borderRadius: 5,
-		overflow: 'hidden',
+		overflow: "hidden",
 		display: "flex",
-		justifyContent: "flex-start",
+		justifyContent: "flex-start"
 	},
 	streamHeader: {
 		paddingHorizontal: 12,
