79f5cd9590dab57da8eabd155563d2233a7471be
Rikki Tissier <rtissier@invisionpower.com>
2018-10-19 14:42:06 -0400

    Fix missing ErrorBox import

--- src/screens/forums/TopicViewScreen.js
+++ src/screens/forums/TopicViewScreen.js
@@ -14,6 +14,7 @@ import TwoLineHeader from "../../atoms/TwoLineHeader";
 import { Post, PostFragment } from "../../ecosystems/Post";
 import Tag from "../../atoms/Tag";
 import TagList from "../../atoms/TagList";
+import ErrorBox from "../../atoms/ErrorBox";
 import Pager from "../../atoms/Pager";
 import PagerButton from "../../atoms/PagerButton";
 import DummyTextInput from "../../atoms/DummyTextInput";
