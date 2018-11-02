06be075cae78e0c3598c69013158afc217938a03
Rikki Tissier <rtissier@invisionpower.com>
2018-11-02 16:30:49 -0400

    Only apply WebView header if it's an internal URL

--- src/screens/core/WebViewScreen.js
+++ src/screens/core/WebViewScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Text, View, WebView } from "react-native";
+import { compose } from "react-apollo";
+import { connect } from "react-redux";
 
-export default class WebViewScreen extends Component {
+class WebViewScreen extends Component {
 	static navigationOptions = ({ navigation }) => ({
 		headerTitle: "..."
 	});
@@ -11,10 +13,22 @@ export default class WebViewScreen extends Component {
 	}
 
 	render() {
+		let headers = {};
+
+		if( this.props.navigation.state.params.url.startsWith( this.props.site.settings.base_url ) ){
+			headers = { 'x-ips-app': 'true' };
+		}
+
 		return (
 			<View style={{ flex: 1 }}>
-				<WebView source={{uri: this.props.navigation.state.params.url, headers: { 'x-ips-app': 'true' } }} style={{ flex: 1 }} />
+				<WebView source={{uri: this.props.navigation.state.params.url, headers }} style={{ flex: 1 }} />
 			</View>
 		);
 	}
 }
+
+export default compose(
+	connect(state => ({
+		site: state.site
+	}))
+)(WebViewScreen);
