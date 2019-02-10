0efd28bbbd4f8de4502b96f29476f917aad09253
Rikki Tissier <rtissier@invisionpower.com>
2019-02-09 20:07:09 -0500

    Android build stuff

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp://u9-pej.rikkitissier.invision-community-app.exp.direct:80";
+  public static final String DEVELOPMENT_URL = "exp://172.122.182.181:19000";
 
 }
