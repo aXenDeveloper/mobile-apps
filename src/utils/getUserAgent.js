f2071a2f1ff7eb33da7ae076942bdb413faa6963
Mark Wade <mwade@invisionpower.com>
2019-04-02 16:46:05 +1100

    Tweaked the user agent

--- src/utils/getUserAgent.js
+++ src/utils/getUserAgent.js
@@ -1,9 +1,7 @@
 export default function getUserAgent() {
 	if (Expo.Constants.platform.ios) {
-		return `InvisionCommunityApp/${Expo.Constants.manifest.version} (iOS|${Expo.Constants.deviceName}|${Expo.Constants.platform.ios.platform}|${
-			Expo.Constants.platform.ios.systemVersion
-		})`;
+		return `InvisionCommunityApp/${Expo.Constants.manifest.version} (${Expo.Constants.manifest.name}) iOS/${Expo.Constants.platform.ios.systemVersion} (${Expo.Constants.platform.ios.platform})`;
 	} else {
-		return `InvisionCommunityApp/${Expo.Constants.manifest.version} (Android|${Expo.Constants.deviceName})`;
+		return `InvisionCommunityApp/${Expo.Constants.manifest.version} (${Expo.Constants.manifest.name}) Android (${Expo.Constants.deviceName})`;
 	}
 }
