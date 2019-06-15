2873294dbf471b433a63acebbafd9879b9191c67
Rikki Tissier <rtissier@invisionpower.com>
2019-06-15 11:31:44 -0400

    Memoize CheckList

--- src/ecosystems/CheckList/index.js
+++ src/ecosystems/CheckList/index.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import { Text, View, FlatList, LayoutAnimation } from "react-native";
+import _ from "underscore";
 
 import CheckListRow from "../../atoms/CheckListRow";
 
-const CheckList = props => (
-	<FlatList
-		data={props.data}
-		renderItem={({ item }) => (
-			<CheckListRow
-				{...item}
-				onPress={() => {
-					LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-					props.onPress(item);
-				}}
-			/>
-		)}
-	/>
-);
+const handlers = {};
+
+const getHandler = (item, onPress) => {
+	if (_.isUndefined(handlers[item.key])) {
+		handlers[item.key] = () => {
+			LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+			onPress(item);
+		};
+	}
+
+	return handlers[item.key];
+};
+const CheckList = props => {
+	return (
+		<FlatList data={props.data} renderItem={({ item }) => <CheckListRow {...item} onPress={!props.disabled ? getHandler(item, props.onPress) : null} />} />
+	);
+};
 
 export default CheckList;
