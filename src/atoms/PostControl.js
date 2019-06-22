92e7cfce61b9a4e7817e28f970c9e259730858de
Rikki Tissier <rtissier@invisionpower.com>
2019-06-22 11:29:11 -0400

    Resolves #34

--- src/atoms/PostControl.js
+++ src/atoms/PostControl.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, TouchableOpacity, StyleSheet, Text, Image } from "react-native";
 import _ from "underscore";
 
+import getImageUrl from "../utils/getImageUrl";
 import styles, { styleVars } from "../styles";
 
 export default class PostControl extends Component {
@@ -16,15 +17,11 @@ export default class PostControl extends Component {
 			return null;
 		}
 
-		if (_.isString(this.props.image) && this.props.image.startsWith("http")) {
-			image = { uri: this.props.image };
-		} else {
-			image = this.props.image;
+		if (_.isString(this.props.image)) {
+			return <Image source={{ uri: getImageUrl(this.props.image) }} style={componentStyles.image} resizeMode="contain" />;
 		}
 
-		if (image) {
-			return <Image source={image} style={componentStyles.image} resizeMode="contain" />;
-		}
+		return <Image source={this.props.image} style={[componentStyles.image, componentStyles.icon]} resizeMode="contain" />;
 	}
 
 	render() {
@@ -36,23 +33,11 @@ export default class PostControl extends Component {
 			>
 				<View
 					testId={this.props.testId}
-					style={[
-						styles.pvTight,
-						styles.flexRow,
-						styles.flexAlignCenter,
-						styles.flexJustifyCenter,
-						this.props.selected ? componentStyles.selected : null
-					]}
+					style={[styles.pvTight, styles.flexRow, styles.flexAlignCenter, styles.flexJustifyCenter, this.props.selected ? componentStyles.selected : null]}
 				>
 					{this.getIcon()}
 					<Text
-						style={[
-							styles.lightText,
-							styles.standardText,
-							styles.mediumText,
-							this.props.selected ? componentStyles.selectedText : null,
-							this.props.textStyle
-						]}
+						style={[styles.lightText, styles.standardText, styles.mediumText, this.props.selected ? componentStyles.selectedText : null, this.props.textStyle]}
 					>
 						{this.props.label}
 					</Text>
@@ -70,7 +55,9 @@ const componentStyles = StyleSheet.create({
 	image: {
 		width: 18,
 		height: 18,
-		marginRight: 4,
+		marginRight: 4
+	},
+	icon: {
 		tintColor: styleVars.lightText
 	},
 	selectedText: {
