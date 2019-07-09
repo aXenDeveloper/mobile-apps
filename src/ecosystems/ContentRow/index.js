08559d6c35308c24fc5a37bd79f9d80b25318981
Rikki Tissier <rtissier@invisionpower.com>
2019-07-09 17:14:22 -0400

    Support an arrow in ContentRow

--- src/ecosystems/ContentRow/index.js
+++ src/ecosystems/ContentRow/index.js
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import ShadowedArea from "../../atoms/ShadowedArea";
-import styles from '../../styles.js';
 
-const stylesheet = StyleSheet.create({
+import icons from "../../icons";
+import styles from "../../styles";
+
+const componentStyles = StyleSheet.create({
 	outerContentRow: {
 		marginBottom: 1
 	},
 	outerContentRowWithSpace: {
 		marginBottom: 4,
 		borderBottomWidth: 1,
-		borderBottomColor: 'rgba(0,0,0,0.05)'
+		borderBottomColor: "rgba(0,0,0,0.05)"
+	},
+	arrow: {
+		width: 18,
+		height: 18
 	}
 });
 
 const ContentRow = props => {
-	const rowClass = props.unread
-		? styles.unreadBackground
-		: styles.readBackground;
+	const rowClass = props.unread ? styles.unreadBackground : styles.readBackground;
 
 	return (
-		<TouchableHighlight
-			style={props.withSpace ? stylesheet.outerContentRowWithSpace : stylesheet.outerContentRow}
-			onPress={props.onPress || null}
-		>
-			<View style={[stylesheet.innerContentRow, rowClass, props.style]}>
-				{props.children}
+		<TouchableHighlight style={props.withSpace ? componentStyles.outerContentRowWithSpace : componentStyles.outerContentRow} onPress={props.onPress || null}>
+			<View style={[styles.flexRow, rowClass]}>
+				<View style={[styles.flexGrow, props.style]}>{props.children}</View>
+				{props.showArrow && (
+					<View style={[styles.flexAlignSelfCenter, styles.mrStandard]}>
+						<Image source={icons.ROW_ARROW} resizeMode="contain" style={componentStyles.arrow} />
+					</View>
+				)}
 			</View>
 		</TouchableHighlight>
 	);
