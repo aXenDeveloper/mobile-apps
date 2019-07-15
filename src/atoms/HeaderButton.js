aXenDeveloper/mobile-apps

commit 2bc7f5c8de25fc898affa75f3f4f41d362bb335a

Provide icons for submit screen; modify HeaderButton to use icon only on android

src/atoms/HeaderButton.js
@@ -7,10 +7,10 @@ import icons from "../icons";
 
 const HeaderButton = props => (
 	<TouchableOpacity style={[componentStyles.wrapper, props.position == "left" ? styles.mlWide : styles.mrWide, props.style]} onPress={props.onPress || null}>
-		{Boolean(props.icon) && (
+		{Boolean(props.icon) && Platform.OS === "android" && (
 			<Image source={props.icon} style={[styles.headerIcon, props.size ? { width: props.size, height: props.size } : componentStyles.defaultSize]} />
 		)}
-		{Boolean(props.label) && <Text style={[styles.headerTitle]}>{props.label}</Text>}
+		{Boolean(props.label) && (!Platform.OS === "android" && !props.alwaysShowIcon) && <Text style={[styles.headerTitle]}>{props.label}</Text>}
 	</TouchableOpacity>
 );
 

src/screens/forums/CreateTopicScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, Alert, Button, TextInput, View, KeyboardAvoidingView, ActivityIndicator } from "react-native";
+import { Text, Alert, Button, TextInput, View, KeyboardAvoidingView, ActivityIndicator, Platform } from "react-native";
 import gql from "graphql-tag";
 import { graphql, compose } from "react-apollo";
 import { NavigationActions, Header } from "react-navigation";
@@ -12,6 +12,7 @@ import { UPLOAD_STATUS } from "../../redux/actions/editor";
 import HeaderButton from "../../atoms/HeaderButton";
 import uniqueID from "../../utils/UniqueID";
 import styles from "../../styles";
+import icons from "../../icons";
 
 const CreateTopicMutation = gql`
 	mutation CreateTopicMutation($forumID: ID!, $title: String!, $content: String!, $tags: [String], $postKey: String!) {
@@ -45,8 +46,10 @@ class CreateTopicScreen extends Component {
 				"New Topic"
 			),
 			headerTintColor: "white",
-			headerLeft: navigation.getParam("submitting") ? null : <HeaderButton position="left" onPress={navigation.getParam("cancelTopic")} label="Cancel" />,
-			headerRight: navigation.getParam("submitting") ? null : <HeaderButton position="right" onPress={navigation.getParam("submitTopic")} label="Post" />
+			//headerLeft: navigation.getParam("submitting") ? null : <HeaderButton position="left" onPress={navigation.getParam("cancelTopic")} label="Cancel" />,
+			headerRight: navigation.getParam("submitting") ? null : (
+				<HeaderButton position="right" onPress={navigation.getParam("submitTopic")} label="Post" icon={icons.SUBMIT} />
+			)
 		};
 	};
 

src/screens/forums/ReplyTopicScreen.js
@@ -16,6 +16,7 @@ import uniqueID from "../../utils/UniqueID";
 import Lang from "../../utils/Lang";
 import relativeTime from "../../utils/RelativeTime";
 import styles from "../../styles";
+import icons from "../../icons";
 
 const ReplyTopicMutation = gql`
 	mutation ReplyTopicMutation($topicID: ID!, $content: String!, $replyingTo: ID) {
@@ -40,8 +41,10 @@ class ReplyTopicScreen extends Component {
 				"Reply"
 			),
 			headerTintColor: "white",
-			headerLeft: navigation.getParam("submitting") ? null : <HeaderButton position="left" label="Cancel" onPress={navigation.getParam("cancelReply")} />,
-			headerRight: navigation.getParam("submitting") ? null : <HeaderButton position="right" label="Post" onPress={navigation.getParam("submitReply")} />
+			//headerLeft: navigation.getParam("submitting") ? null : <HeaderButton position="left" label="Cancel" onPress={navigation.getParam("cancelReply")} />,
+			headerRight: navigation.getParam("submitting") ? null : (
+				<HeaderButton position="right" label="Post" icon={icons.SUBMIT} onPress={navigation.getParam("submitReply")} />
+			)
 		};
 	};
 

commit 295f6f9390fb66e42a59e71b1e6bb68f3ff3e05b

Fix an issue I inadvertently introduced in 2bc7f5c8 where iOS showed no icon or label in HeaderButton

src/atoms/HeaderButton.js
@@ -7,10 +7,10 @@ import icons from "../icons";
 
 const HeaderButton = props => (
 	<TouchableOpacity style={[componentStyles.wrapper, props.position == "left" ? styles.mlWide : styles.mrWide, props.style]} onPress={props.onPress || null}>
-		{Boolean(props.icon) && Platform.OS === "android" && (
+		{Boolean(props.icon) && (Platform.OS === "android" || props.alwaysShowIcon) && (
 			<Image source={props.icon} style={[styles.headerIcon, props.size ? { width: props.size, height: props.size } : componentStyles.defaultSize]} />
 		)}
-		{Boolean(props.label) && (!Platform.OS === "android" && !props.alwaysShowIcon) && <Text style={[styles.headerTitle]}>{props.label}</Text>}
+		{Boolean(props.label) && (Platform.OS !== "android" || !props.alwaysShowIcon) && <Text style={[styles.headerTitle]}>{props.label}</Text>}
 	</TouchableOpacity>
 );
 