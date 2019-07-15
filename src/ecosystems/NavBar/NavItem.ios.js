318ae07dbf8ae029dfe232c0df4db240e9257e79
Rikki Tissier <rtissier@invisionpower.com>
2019-07-15 17:29:48 -0400

    Fix style regression on NavItem for iOS

--- src/ecosystems/NavBar/NavItem.ios.js
+++ src/ecosystems/NavBar/NavItem.ios.js
@@ -4,10 +4,10 @@ import { View, TouchableOpacity, StyleSheet, Image, Text } from "react-native";
 import styles, { styleVars } from "../../styles";
 
 const NavItem = props => (
-	<TouchableOpacity style={componentStyles.navItem} onPress={props.onPress}>
-		<View style={[styles.flexRow, styles.flexAlignCenter, styles.phWide, styles.pvStandard, styles.mlStandard]}>
+	<TouchableOpacity style={[styles.mlStandard, styles.lightBackground, styles.phWide, styles.pvStandard, componentStyles.navItem]} onPress={props.onPress}>
+		<View style={[styles.flexRow, styles.flexAlignCenter, styles.flexJustifyCenter]}>
 			<Image source={props.icon} resizeMode="contain" style={[styles.mrTight, componentStyles.navItemIcon]} />
-			<Text style={[styles.smallText, styles.mediumText, componentStyles.navItemText]}>{props.title}</Text>
+			<Text style={[styles.smallText, styles.mediumText, styles.standardText, componentStyles.navItemText]}>{props.title}</Text>
 		</View>
 	</TouchableOpacity>
 );
@@ -16,17 +16,15 @@ export default NavItem;
 
 const componentStyles = StyleSheet.create({
 	navItem: {
-		backgroundColor: "#F5F5F5",
 		borderRadius: 30
 	},
 	navItemText: {
-		lineHeight: 13,
-		color: styleVars.tabActive
+		color: styleVars.tabActive,
+		marginTop: -1
 	},
 	navItemIcon: {
 		width: 14,
 		height: 14,
-		tintColor: styleVars.tabActive,
-		marginTop: -1
+		tintColor: styleVars.tabActive
 	}
 });
