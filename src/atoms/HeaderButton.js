f118d8fe897e9e52c1bc73ac425c06a24a7d7ed1
Rikki Tissier <rtissier@invisionpower.com>
2019-07-15 22:55:53 -0400

    My HeaderButton logic was still wrong... this should do it.

--- src/atoms/HeaderButton.js
+++ src/atoms/HeaderButton.js
@@ -5,14 +5,31 @@ import _ from "underscore";
 import styles, { styleVars } from "../styles";
 import icons from "../icons";
 
-const HeaderButton = props => (
-	<TouchableOpacity style={[componentStyles.wrapper, props.position == "left" ? styles.mlWide : styles.mrWide, props.style]} onPress={props.onPress || null}>
-		{Boolean(props.icon) && (Platform.OS === "android" || props.alwaysShowIcon) && (
-			<Image source={props.icon} style={[styles.headerIcon, props.size ? { width: props.size, height: props.size } : componentStyles.defaultSize]} />
-		)}
-		{Boolean(props.label) && (Platform.OS !== "android" || !props.alwaysShowIcon) && <Text style={[styles.headerTitle]}>{props.label}</Text>}
-	</TouchableOpacity>
-);
+const HeaderButton = props => {
+	let showIcon = false;
+	let showLabel = false;
+
+	if (props.icon) {
+		if (Platform.OS === "android" || props.alwaysShowIcon || !props.label) {
+			showIcon = true;
+		}
+	}
+
+	if (props.label) {
+		if (Platform.OS === "ios" || !props.icon) {
+			showLabel = true;
+		}
+	}
+
+	return (
+		<TouchableOpacity style={[componentStyles.wrapper, props.position == "left" ? styles.mlWide : styles.mrWide, props.style]} onPress={props.onPress || null}>
+			{showIcon && (
+				<Image source={props.icon} style={[styles.headerIcon, props.size ? { width: props.size, height: props.size } : componentStyles.defaultSize]} />
+			)}
+			{showLabel && <Text style={[styles.headerTitle]}>{props.label}</Text>}
+		</TouchableOpacity>
+	);
+};
 
 export default HeaderButton;
 
