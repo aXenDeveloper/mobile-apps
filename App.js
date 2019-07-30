1586f7acb28635e4350d98d4831112c776ee237d
Rikki Tissier <rtissier@invisionpower.com>
2019-07-30 14:42:33 -0400

    Implement Expo.AppLoading to speed up rendering images. Resolves #41

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { Provider } from "react-redux";
 import configureStore from "./src/redux/configureStore";
 import { connect } from "react-redux";
 import Sentry from "sentry-expo";
+import { Asset } from "expo-asset";
+import { AppLoading } from "expo";
 
 global.Buffer = global.Buffer || require("buffer").Buffer;
 
 import AppRoot from "./src/screens/ips/AppRoot";
+import icons, { splashImage, illustrations, navigationIcons } from "./src/icons";
 
 const store = configureStore();
 
@@ -24,9 +27,31 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+		this.state = {
+			isReady: false
+		};
+	}
+
+	async cacheResourcesAsync() {
+		// Get the key:value icons etc.
+		const imagesToCache = [icons, illustrations, navigationIcons].map(obj => Object.values(obj)).flat();
+
+		// Add splash screen
+		imagesToCache.push(splashImage);
+
+		// Now download
+		const cacheImages = imagesToCache.map(image => {
+			return Asset.fromModule(image).downloadAsync();
+		});
+
+		return Promise.all(cacheImages);
 	}
 
 	render() {
+		if (!this.state.isReady) {
+			return <AppLoading startAsync={this.cacheResourcesAsync} onFinish={() => this.setState({ isReady: true })} onError={msg => console.log(msg)} />;
+		}
+
 		return (
 			<Provider store={store}>
 				<AppRoot />
