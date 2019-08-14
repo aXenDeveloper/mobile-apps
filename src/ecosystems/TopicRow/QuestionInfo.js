937f180e3a5f4f7d69b97853a6ebb8dce0724d18
Rikki Tissier <rtissier@invisionpower.com>
2019-08-14 13:52:05 -0400

    Remove topic icon

--- src/ecosystems/TopicRow/QuestionInfo.js
+++ src/ecosystems/TopicRow/QuestionInfo.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Text, Image, View, StyleSheet, TouchableHighlight } from "react-native";
 
 import Lang from "../../utils/Lang";
-import { PlaceholderElement, PlaceholderContainer } from "../../ecosystems/Placeholder";
-import TopicIcon from "../../atoms/TopicIcon";
 import UnreadIndicator from "../../atoms/UnreadIndicator";
 import LockedIcon from "../../atoms/LockedIcon";
 import { withTheme } from "../../themes";
@@ -12,7 +10,6 @@ const QuestionInfo = ({ styles, componentStyles, rowStyles, ...props }) => (
 	<View style={[rowStyles.topicRowInner, componentStyles.questionRowInner]}>
 		<View style={rowStyles.topicInfo}>
 			<View style={rowStyles.topicTitle}>
-				{Boolean(props.showAsUnread) && <TopicIcon style={rowStyles.topicIcon} unread={props.data.unread} />}
 				{Boolean(props.data.isLocked) && <LockedIcon style={rowStyles.lockedIcon} />}
 				<Text style={[rowStyles.topicTitleText, props.showAsUnread ? styles.title : styles.titleRead]} numberOfLines={1}>
 					<UnreadIndicator show={props.data.unread} />
