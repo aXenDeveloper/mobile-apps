b7159d6e5779734da5a420d7f96619fe591793e7
Rikki Tissier <rtissier@invisionpower.com>
2019-08-31 10:16:17 -0400

    Modify editor contents to match ckeditor paragraphs. Resolves #142

--- src/utils/richText.js
+++ src/utils/richText.js
@@ -11,5 +11,17 @@ export function processToSend(content) {
 	// Remove invisible chars that quill adds
 	content = content.replace(/\uFEFF/g, "");
 
+	// Quill adds a <p><br></p> when enter is pressed, resulting in double line
+	// spacing on the web. If we have two <p><br></p> next to each other, replace
+	// it with a single line-space like we do on the web, but if it's just one
+	// we can remove it because normal <p> styling takes care of the spacing
+	// If we have two paragraphs right next to each other, then turn that into a
+	// single linespace (same as pressing shift-enter on web)
+	content = content.replace(/<p><br><\/p>/g, "~~//br//~~");
+	content = content.replace(/<\/p><p>/g, "<br>");
+	content = content.replace(/~~\/\/br\/\/~~/g, "<p><br></p>");
+	content = content.replace(/<p><br><\/p><p><br><\/p>/g, "<p>&nbsp;</p>");
+	content = content.replace(/<p><br><\/p>/g, "");
+
 	return content;
 }
