20725bb8877445488658ce538332ddaa2f44c8c7
Rikki Tissier <rtissier@invisionpower.com>
2019-11-01 17:28:29 -0400

    When looking for content images we need to ensure https, per apple requirements

--- src/utils/getSuitableImage.js
+++ src/utils/getSuitableImage.js
@@ -1,14 +1,18 @@
 import _ from "underscore";
 
 export default function getSuitableImage(data) {
-	/*let imageToUse = false;
-	for( let i = 0; i < data.length; i++ ){
-		if( data[i].indexOf('https') === 0 ){
+	let imageToUse = false;
+
+	if (!_.isArray(data)) {
+		return imageToUse;
+	}
+
+	for (let i = 0; i < data.length; i++) {
+		// We need either https, or protocol-relative (which getImageUrl will transform to https later)
+		if (data[i].startsWith("https") || data[i].startsWith("//")) {
 			imageToUse = data[i];
 			break;
 		}
 	}
-	return imageToUse;*/
-
-	return data !== null && data.length ? data[0] : false;	
+	return imageToUse;
 }
