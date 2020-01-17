ca066c0b017fafa0fc33287ef8729cad6dfa47d8
Rikki Tissier <rtissier@invisionpower.com>
2020-01-17 13:03:10 +0000

    Reactotron config

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -3,16 +3,18 @@ import Reactotron, { networking } from "reactotron-react-native";
 import { reactotronRedux } from "reactotron-redux";
 
 const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-	.configure({ host: "192.168.1.33", port: 9999 }) // controls connection & communication settings
+	.configure({ host: "192.168.1.100", port: 9999 }) // controls connection & communication settings
 	.useReactNative() // add all built-in react native plugins
 	.use(
 		networking({
 			ignoreUrls: /\/(logs|symbolicate)$/
 		})
 	)
-	.use(reactotronRedux())
-	.connect(); // let's connect!
+	.use(reactotronRedux());
 
+//if (__DEV__) {
+reactotron.connect(); // let's connect!
 console.tron = Reactotron;
+//}
 
 export default reactotron;
