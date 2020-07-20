1412ebde201cd5d35b59881eaea2c0a281c3ea5e
Rikki Tissier <rtissier@invisionpower.com>
2020-07-20 16:56:11 -0400

    Update reactotron config

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,5 +1,5 @@
 import { AsyncStorage } from "react-native";
-import Reactotron, { networking } from "reactotron-react-native";
+import Reactotron, { networking, asyncStorage, trackGlobalErrors } from "reactotron-react-native";
 import { reactotronRedux } from "reactotron-redux";
 
 const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
@@ -10,7 +10,13 @@ const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStora
 			ignoreUrls: /\/(logs|symbolicate)$/
 		})
 	)
-	.use(reactotronRedux());
+	.use(reactotronRedux())
+	.use(
+		trackGlobalErrors({
+			veto: frame => frame.fileName.indexOf("/node_modules/react-native/") >= 0
+		})
+	)
+	.use(asyncStorage());
 
 //if (__DEV__) {
 reactotron.connect(); // let's connect!
